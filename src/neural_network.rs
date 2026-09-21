9a1d348e07684e0e01429415c5ca039d3605c569
bengillitt <bengillitt@proton.me>
2026-09-21 12:45:50 +0100

    added forward pass

--- src/neural_network.rs
+++ src/neural_network.rs
@@ -2,6 +2,8 @@ pub mod layer;
 
 pub use super::matrix_math;
 
+use matrix_math::FlatMatrix;
+
 use layer::Layer;
 
 pub struct Network {
@@ -9,4 +11,20 @@ pub struct Network {
     softmax_enabled: bool,
     weight_decay: Option<f64>,
     matrix: matrix_math::FlatMatrix,
+}
+
+impl Network {
+    pub fn forward_pass(&self, inputs: FlatMatrix) -> FlatMatrix {
+        let mut input = inputs;
+
+        for i in 0..self.layers.len() {
+            input = self.layers[i].pass(&input);
+        }
+
+        if self.softmax_enabled {
+            
+        }
+
+        return input;
+    }
 }
